87cd67764abb07e4c4aacac01816bafbfad5cfaa
Andi-Lo <contact@andreaslorer.de>
2016-10-26 17:48:05 +0200

    changed function to use turf

--- src/js/modules/grid.js
+++ src/js/modules/grid.js
@@ -1,33 +1,41 @@
 var turf = require('@turf/turf');
+turf.meta = require('@turf/meta');
 var draw = require('./draw');
 
 module.exports = function(ctx) {
-  var bbox = [-87.95654296875, 9.709057068618208, -62.75390625, 29.19053283229458];
+  var bbox = [
+    -87.95654296875,
+    9.709057068618208,
+    -62.75390625,
+    29.19053283229458
+  ];
 
   var cellWidth = 25;
   var units = 'miles';
 
   var grid = turf.pointGrid(bbox, cellWidth, units);
 
-  fetch('./map/map.geojson').then((res) => res.json()).then((geo) => {
-    var count = 0;
+  // draw raster map
+  fetch('./map/map.geojson').then((parseJson) => parseJson.json()).then((props) => {
     // draw map
-    geo.features.forEach((feat) => {
-      const {type, coordinates} = feat.geometry;
+    turf.meta.featureEach(props, function(feature) {
+      var type = feature.geometry.type;
 
       switch (type) {
-
-      case "Polygon":
-        grid.features.forEach((feature) => {
-          const {type, coordinates} = feature.geometry;
-          switch (type) {
-          case "Point":
-            if(turf.inside(feature, feat)) {
-              draw.drawPoint(ctx, coordinates, 'rgba(50, 80, 0, 0.4)', 0.1);
+        case 'Polygon':
+          turf.meta.featureEach(grid, function(polygon) {
+            switch (polygon.geometry.type) {
+              case "Point":
+                if (turf.inside(polygon, feature)) {
+                  draw.drawPoint(ctx, polygon.geometry.coordinates, 'rgba(50, 80, 0, 0.4)', 0.1);
+                }
+                break;
             }
-          }
-        });
-        break;
+          });
+          break;
+
+        default:
+          break;
       }
     });
   });
