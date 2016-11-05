0444ab61af76d2d7e06ca43c339967a53739800b
Andi-Lo <contact@andreaslorer.de>
2016-11-05 22:37:02 +0100

    refactored code

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,10 @@
 function shipcruising(options) {
   'use strict';
 
-  // var _ = require('underscore');
   var createMap = require('./modules/createMap');
   var draw = require('./modules/draw');
-  var grid = require('./modules/grid');
-  var turf = require('./modules/turf');
   var el = window.document.getElementById('ship-cruising');
-
+  var rgb2hex = require('rgb2hex');
   var defaults = {
     'strokeColor': 'rgba(255, 80, 255, 0.8)',
     'pointColor': 'rgba(255, 50, 10, 0.8)',
@@ -19,37 +16,56 @@ function shipcruising(options) {
 
   var canvas = createMap(defaults.width, defaults.height);
   var ctx = canvas.getContext('2d');
-  var grid = grid(ctx);
   el.appendChild(canvas);
 
-  fetch('./map/map.geojson').then((parse) => parse.json()).then((geo) => {
-    // draw map
+  fetch('./map/jamaica.geojson').then((parse) => parse.json()).then((geo) => {
+    // console.log(geo);
     geo.features.forEach((features) => {
       switch (features.geometry.type) {
         case "Polygon":
-          turf.meta.featureEach(features, function(coords) {
-            draw.drawMultiPolygon(ctx, coords, defaults.mapColor);
-          });
+          draw.drawPolygon(ctx, features, defaults.mapColor);
+          break;
+
+        case "Point":
+          draw.drawPoint(ctx, features, defaults.pointColor, 4);
+          break;
+
+        case "MultiPolygon":
+          draw.drawMultiPolygon(ctx, features, defaults.mapColor);
+          break;
+
+        default:
+          console.log(features.geometry.type);
           break;
       }
     });
+    var color = ctx.getImageData(1, 1, 1, 1);
+    var hex = rgb2hex('rgba(' + color.data +')');
+    console.log('color rgba', color.data, 'color in hex', hex);
+
+    // var colorData = new Array(640);
+    // for (var i = 0; i < 640; i++) {
+    //   colorData[i] = new Array(640);
+    // }
+
+    // for(var y = 0; y < 640; y++) {
+    //   for(var x = 0; x < 640; x++) {
+    //     color = ctx.getImageData(x, y, 1, 1);
+    //     var hex = rgb2hex('rgba(' + color.data +')');
+    //     colorData[x][y] = hex;
+    //   }
+    // }
+    // console.log('data', colorData[640-1][640-1]);
   });
 
   fetch('./map/route.geojson').then((parse) => parse.json()).then((geo) => {
-    // draw points
     geo.features.forEach((features) => {
       switch (features.geometry.type) {
         case "Point":
-          draw.drawPoint(
-            ctx,
-            features.geometry.coordinates,
-            defaults.pointColor,
-            4
-          );
+          draw.drawPoint(ctx, features, defaults.pointColor, 4);
           break;
       }
     });
-
     draw.drawRoute(ctx, geo, defaults.strokeColor);
   });
 };
--- src/js/modules/draw.js
+++ src/js/modules/draw.js
@@ -3,80 +3,92 @@
 var posToPixel = require('./positionToPixel');
 var turf = require('./turf');
 
-// todo
-var drawMultiPolygon = function(ctx, featureCollection, color) {
-  // console.log('featureCollection', featureCollection);
+var unpackMultiPolCoords = function(features) {
+  var data = [];
+
+  turf.meta.featureEach(features, function(feature) {
+    var coordCollection = feature.geometry.coordinates;
+    coordCollection.forEach(function(coords) {
+      coords.forEach(function(coord) {
+        data.push(coord);
+      });
+    });
+  });
+  return data;
+};
+
+var drawLine = function(ctx, coord, isFirst, stroke = false) {
+  var pixel = posToPixel(coord);
+
+  if(pixel.x > 0 || pixel.y > 0) {
+    if(isFirst === true) {
+      ctx.beginPath();
+      ctx.moveTo(pixel.x, pixel.y);
+      isFirst = false;
+    }
+    else {
+      ctx.lineTo(pixel.x, pixel.y);
+      if (stroke === true) {
+        ctx.stroke();
+      }
+    }
+  }
+  return isFirst;
+};
+
+var drawMultiPolygon = function(ctx, features, color) {
   ctx.fillStyle = color;
   var isFirst = true;
 
-  turf.meta.coordEach(featureCollection, function(coord) {
-    var pixel = posToPixel(coord);
-
-    if(pixel.x > 0 || pixel.y > 0) {
-      if(isFirst === true) {
-        ctx.beginPath();
-        ctx.moveTo(pixel.x, pixel.y);
-        isFirst = false;
-      }
-      else {
-        ctx.lineTo(pixel.x, pixel.y);
-      }
-    }
+  var coordinates = unpackMultiPolCoords(features);
+  coordinates.forEach(function(coords) {
+    coords.forEach(function(coord) {
+      isFirst = drawLine(ctx, coord, isFirst);
+    });
+    isFirst = true;
+    ctx.closePath();
+    ctx.fill();
   });
 
-  isFirst = true;
   ctx.closePath();
   ctx.fill();
 };
 
-var drawPolygon = function(ctx, feature, color) {
-  // console.log('feature', feature);
+var drawPolygon = function(ctx, features, color) {
   ctx.fillStyle = color;
-
   var isFirst = true;
 
-  turf.meta.coordEach(feature, function(coord) {
-    var pixel = posToPixel(coord);
-
-    if(pixel.x > 0 || pixel.y > 0) {
-      if(isFirst === true) {
-        ctx.beginPath();
-        ctx.moveTo(pixel.x, pixel.y);
-        isFirst = false;
-      }
-      else {
-        ctx.lineTo(pixel.x, pixel.y);
-      }
-    }
+  turf.meta.featureEach(features, function(feature) {
+    turf.meta.coordEach(feature, function(coord) {
+      isFirst = drawLine(ctx, coord, isFirst);
+    });
   });
 
-  isFirst = true;
   ctx.closePath();
   ctx.fill();
 };
 
-var drawPoint = function(ctx, point, color, lineWidth) {
-  // make points visible
-  ctx.globalCompositeOperation='destination-over';
-
+var drawPoint = function(ctx, features, color, lineWidth) {
+  // console.log('point feautes', features);
+  var point = features.geometry.coordinates;
+  var pixel = posToPixel(point);
+  ctx.globalCompositeOperation = 'destination-over';
   ctx.fillStyle = color;
   ctx.strokeStyle = color;
   ctx.lineWidth = lineWidth;
-
-  var pixel = posToPixel(point);
 
   ctx.beginPath();
   ctx.arc(pixel.x, pixel.y, lineWidth, 0, (Math.PI/180)*360, false);
   ctx.stroke();
   ctx.closePath();
 
-  // draw full circle
+  // fill circle
   ctx.fill();
 };
 
-var drawRoute = function(ctx, route, color, fill = false) {
-  // console.log('route', route);
-  var length = route.features.length;
+var drawRoute = function(ctx, route, color) {
+  var routeLength = route.features.length;
+  var length = 1;
   var isFirst = true;
   ctx.fillStyle = color;
   ctx.strokeStyle = color;
@@ -84,28 +96,11 @@ var drawRoute = function(ctx, route, color, fill = false) {
   ctx.globalCompositeOperation = 'destination-over';
 
   turf.meta.featureEach(route, function(point) {
-    // console.log("point", point);
     var coord = turf.invariant.getCoord(point);
-    // console.log("coord", coord);
-    var pixel = posToPixel(coord);
-    // console.log("pixel", pixel);
-
-    if(pixel.x > 0 && pixel.y > 0) {
-      if(isFirst === true) {
-        ctx.beginPath();
-        ctx.moveTo(pixel.x, pixel.y);
-        isFirst = false;
-      }
-      else {
-        ctx.lineTo(pixel.x, pixel.y);
-        ctx.stroke();
-      }
-    }
-    if(isFirst === length) {
-      ctx.closePath();
-    }
+    isFirst = drawLine(ctx, coord, isFirst, true);
+    if(routeLength === length) ctx.closePath();
+    length++;
   });
-  fill === true ? ctx.fill() : '';
 };
 
 exports.drawPoint = drawPoint;
