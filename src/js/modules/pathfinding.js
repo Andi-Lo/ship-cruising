03a8fd797b9cdd394b12d06499d186efd6a07fd8
Andi-Lo <contact@andreaslorer.de>
2016-11-10 15:54:44 +0100

    tidy up data with simplify-js lib

--- src/js/modules/pathfinding.js
+++ src/js/modules/pathfinding.js
@@ -1,20 +1,34 @@
 var astar = require('../libs/astar.js');
+var turf = require('./turf');
+var simplify = require('simplify-js');
+var draw = require('./draw');
 
 module.exports = function(canvas, colorData, start, end) {
-  var graph = new astar.Graph(colorData);
+  var graph = new astar.Graph(colorData, {diganoal: true});
   var start = graph.grid[start.x][start.y];
   var end = graph.grid[end.x][end.y];
   var result = astar.astar.search(graph, start, end);
 
-  drawPixels(canvas, result);
+  var simple = tidy(result);
+  draw.drawPixels(canvas, simple);
 
   return result;
 };
 
-function drawPixels(canvas, path, color = 'rgba(670, 160, 50, 0.8)') {
-  var ctx = canvas.getContext('2d');
-  ctx.fillStyle = color;
-  path.forEach((pixel) => {
-    ctx.fillRect(pixel.x, pixel.y, 2, 2);
+function tidy(data, tolerance = 1) {
+  // change data format to [{num.x, num.y},{..},{.}] for simplify func
+  var longLat = [];
+  data.forEach((pixelPos) => {
+    longLat.push({'x': pixelPos.x, 'y': pixelPos.y});
   });
+
+  var simplified = simplify(longLat, tolerance, true);
+
+  // get the data back to format [[0 => long, 1 => lat], .., .]
+  var toArray = [];
+  simplified.forEach((obj) => {
+    toArray.push([obj.x, obj.y]);
+  });
+
+  return toArray;
 };
