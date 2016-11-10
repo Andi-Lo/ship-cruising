82a48a554f5682fd2fe5394764d69e211f7542cb
Andi-Lo <contact@andreaslorer.de>
2016-11-10 12:22:30 +0100

    refactored pathfinding method

--- src/js/modules/pathfinding.js
+++ src/js/modules/pathfinding.js
@@ -1,22 +1,20 @@
 var astar = require('../libs/astar.js');
 
 module.exports = function(canvas, colorData, start, end) {
-  var ctx = canvas.getContext('2d');
-
-  findPath(ctx, colorData, start, end);
-};
-
-function drawPixels(ctx, path) {
-  ctx.fillStyle = 'rgba(670, 160, 50, 0.8)';
-  path.forEach((pixel) => {
-    ctx.fillRect(pixel.x, pixel.y, 2, 2);
-  });
-};
-
-function findPath(ctx, colorData, start, end) {
   var graph = new astar.Graph(colorData);
   var start = graph.grid[start.x][start.y];
   var end = graph.grid[end.x][end.y];
   var result = astar.astar.search(graph, start, end);
-  drawPixels(ctx, result);
+
+  drawPixels(canvas, result);
+
+  return result;
+};
+
+function drawPixels(canvas, path, color = 'rgba(670, 160, 50, 0.8)') {
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = color;
+  path.forEach((pixel) => {
+    ctx.fillRect(pixel.x, pixel.y, 2, 2);
+  });
 };
