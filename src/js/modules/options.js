4f502513e63d6c09ee4a009e7c081ec8e75edd79
Andi-Lo <contact@andreaslorer.de>
2016-11-13 16:09:10 +0100

    added colors

--- src/js/modules/options.js
+++ src/js/modules/options.js
@@ -1,8 +1,9 @@
 'use strict';
 
 let defaults = {
-  'strokeColor': 'rgba(255, 80, 255, 0.8)',
+  'strokeColor': 'rgba(670, 160, 50, 0.8)',
   'pointColor': 'rgba(255, 50, 10, 0.8)',
+  'pixelColor': 'rgba(50, 200, 0, 0.8)',
   'mapColor': '#303030',
   'width': 640,
   'height': 640
