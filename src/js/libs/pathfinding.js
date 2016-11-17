0f74a7e9d6cb9b4cc4c7cc9f114338ae91d1a85c
Andi-Lo <contact@andreaslorer.de>
2016-11-17 21:51:30 +0100

    should fix issue Close gaps in Graph #5

--- src/js/libs/pathfinding.js
+++ src/js/libs/pathfinding.js
@@ -4,20 +4,29 @@ let turf = require('./turf');
 let mercator = require('./mercator');
 let canvasMap = require('../modules/canvasMap');
 let simplifyjs = require('simplify-js');
+var _ = require('lodash/array');
 
 let colorData = [];
 
-module.exports = function(fc) {
+/**
+ * Pathfinding uses the Astar algorithm to find the
+ * route. Input has to be a geojson featureCollection containing each
+ * harbour as a feature object with long / lat coordinates
+ * @param {any} featureCollection
+ * @returns returns a featureCollection containing lineString features
+ */
+module.exports = function(featureCollection) {
   colorData = canvasMap.getColorData();
-  let feature = turf.iterateFeature(fc);
-
-  let start = feature.next();
+  let feature = turf.iterateFeature(featureCollection);
+  let prevPoint = false;
   let lineCollection = [];
+  let start = feature.next();
 
-  for(let i = 0; i < (fc.features.length - 1); i++) {
+  for(let i = 0; i < (featureCollection.features.length - 1); i++) {
     let next = feature.next();
     if(next.done !== true) {
-      let path = findPath(start, next);
+      let path = findPath(start, next, prevPoint);
+      prevPoint = _.last(path.route.geometry.coordinates);
       path.route.properties.name = 'route';
       start = path.prev;
       lineCollection.push(path.route);
@@ -26,21 +35,24 @@ module.exports = function(fc) {
       break;
     }
   }
-  lineCollection = turf.featureCollection(lineCollection);
-  return lineCollection;
+  return turf.featureCollection(lineCollection);
 };
 
-function findPath(start, end) {
+/**
+ * Use astar to find path from "start" to "end"
+ * @param {any} start
+ * @param {any} end
+ * @returns an {route, prev} object containing the path A to B and the last harbour B
+ */
+function findPath(start, end, prevPoint) {
   let graph = new Graph(colorData, {diagonal: true});
   let heuristic = {heuristic: astar.heuristics.diagonal};
   let prev = end;
   let path;
-
   let getNode = (node) => {
     node = mercator.posToPixel(node.value.geometry.coordinates);
     return graph.grid[node.x][node.y];
   };
-
   start = getNode(start);
   end = getNode(end);
 
@@ -54,23 +66,40 @@ function findPath(start, end) {
     throw error;
   }
   let route = simplifyPath(path);
+  if(prevPoint !== false) route.geometry.coordinates[0] = prevPoint;
   return {route, prev};
 };
 
-function simplifyPath(path) {
+
+/**
+ * Creates a smooth bezier curve and simplifies it
+ * @param {any} path
+ * @param {any} prevPoint
+ * @param {number} [sharpness=0.4]
+ * @returns bezier curve feature of type lineString
+ * TODO: parameterize function for later control usage. Probably use some
+ * form of option object to not get an endless list of parameters
+ */
+function simplifyPath(path, sharpness = 0.4) {
   let simplified = simplify(path);
   let linestring = turf.lineString(simplified);
 
   const resolution = 10000;
-  const sharpness = .4;
   let bezier = turf.bezier(linestring, resolution, sharpness);
   let bezierSimplified = turf.simplify(bezier, 0.01, false);
 
   return bezierSimplified;
 };
 
-function simplify(path, tolerance = 4) {
-  // change path format to [{num.x, num.y},{..},{.}] for simplify func
+/**
+ * Simplify uses simplify-js to compress the route to a smaller dataset using
+ * a pixel tolerance as a parameter.
+ * @param {any} path
+ * @param {number} [tolerance=3]
+ * @returns an array containing the simplified data set
+ */
+function simplify(path, tolerance = 3) {
+  // change path format from [[x,y],[..],[.]] to [{x, y},{..},{.}] for simplify func
   let longLat = [];
   path.forEach((pixelPos) => {
     longLat.push({'x': pixelPos.x, 'y': pixelPos.y});
