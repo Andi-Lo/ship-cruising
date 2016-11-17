f92d92e9d566e1e4d4684737d7fba22d6c3511dd
Andi-Lo <contact@andreaslorer.de>
2016-11-17 13:49:35 +0100

    added keyboard observer

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
 'use strict';
 
-function shipcruising() {
-  let canvasMap = require('./modules/canvasMap');
-  let leafletMap = require('./modules/leafletMap');
-  let defaults = require('./modules/options').defaults;
+let KeyboardObserver = require('./observers/KeyboardObserver').KeyboardObserver;
+let canvasMap = require('./modules/canvasMap');
+let leafletMap = require('./modules/leafletMap');
+let defaults = require('./modules/options').defaults;
 
+function shipcruising() {
   let elInteractive = window.document.getElementById('interactive-map');
+
+  new KeyboardObserver();
 
   canvasMap.createMap(defaults.width, defaults.height);
   canvasMap.init('./map/jamaica.geojson');
---
+++ src/js/observers/keyboardObserver.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let Observer = require('./observer').Observer;
+let Route = require('../modules/route').Route;
+let drawLeaflet = require('../modules/drawLeaflet');
+let canvasMap = require('../modules/canvasMap');
+
+class KeyboardObserver extends Observer {
+  constructor() {
+    super();
+    window.addEventListener("keydown", function(evt) {
+      if(evt.defaultPrevented) {
+        return; // Do nothing if the evt was already processed
+      }
+
+      switch (evt.key) {
+        case "Enter":
+          try {
+            let features = canvasMap.getFeatures();
+            let route = new Route(features);
+            route.drawRoute(route._route);
+            drawLeaflet.drawPolyline(route._route);
+            drawLeaflet.drawMarkers(route._waypoints);
+
+            canvasMap.setFeatures([]);
+          }
+          catch(e) {
+            throw e;
+          }
+          finally {
+            canvasMap.setFeatures([]);
+          }
+          break;
+        default:
+          return; // Quit when this doesn't handle the key evt.
+      }
+      // Cancel the default action to avoid it being handled twice
+      evt.preventDefault();
+    }, true);
+  }
+
+}
+
+module.exports.KeyboardObserver = KeyboardObserver;
+
