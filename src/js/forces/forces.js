cde0afa2d2cfbe6d28455853d3934f08eeffe5b9
Andi-Lo <contact@andreaslorer.de>
2016-12-08 12:34:26 +0100

    update forces with stackoverflow answer

--- src/js/forces/forces.js
+++ src/js/forces/forces.js
@@ -4,49 +4,6 @@ let mercator = require('../libs/mercator');
 let turf = require('../libs/turf');
 let d3 = require("d3");
 
-let simulation;
-let mbStrength = 1;
-let _route = [];
-let distElem = window.document.getElementById('distanceElem');
-let strengthElem = window.document.getElementById('strengthElem');
-let mbStrengthElem = window.document.getElementById('mbStrengthElem');
-let distance;
-let strength = strengthElem.value;
-distance = distElem.value;
-
-// strengthElem.addEventListener('click', function(evt) {
-//   strength = strengthElem.value;
-//   distance = distElem.value;
-//   console.log('strength', strength);
-//   // d3.select('force').selectAll("*").remove();
-//   // simulation.alphaTarget(0);
-//   simulation.alphaTarget(0.3).restart();
-//   // force(_route, distance, strength);
-// }, false);
-
-mbStrengthElem.addEventListener('click', function(evt) {
-  mbStrength = mbStrengthElem.value;
-  // d3.select('force').selectAll("*").remove();
-  console.log('strength', mbStrength);
-  // simulation.alphaTarget(0);
-  simulation.alphaTarget(0.3).restart();
-  // force(_route, distance, strength);
-}, false);
-
-distElem.addEventListener('click', function(evt) {
-  strength = strengthElem.value;
-  distance = distElem.value;
-  console.log('dist', distance);
-  // d3.select('force').selectAll("*").remove();
-  // simulation.alphaTarget(0);
-  simulation.alphaTarget(0.3).restart();
-  // force(_route, distance, strength);
-}, false);
-
-let clearForces = function() {
-  d3.select('force').selectAll("*").remove();
-};
-
 let createNode = function(coords) {
   return {
     radius: 4,
@@ -85,22 +42,38 @@ let getLinks = (nodes) => {
   return obj;
 };
 
-let force = function(route, distance = 1, strength = 2) {
-  _route = route;
+let force = function(route) {
   let width = 960;
   let height = 500;
   let nodes = getNodes(route);
   let links = getLinks(nodes);
 
-  simulation = d3.forceSimulation().nodes(nodes)
-    .force("link", d3.forceLink()
-      .id(function(d) { return d.index; })
-      .strength(function(d) { return strength; })
-      .distance(function(d) { return distance; }))
-    .force("charge", d3.forceManyBody()
-      .strength(function(d) { return mbStrength; })
-      .distanceMax(function(d) { return 10; })
-      .distanceMin(function(d) { return 1; }));
+  d3.select("#strengthElem").on("input", function() {
+    linkForce.strength(this.value);
+    simulation.alpha(0.5).restart();  // Re-heat the simulation
+  });
+
+  d3.select("#distanceElem").on("input", function() {
+    linkForce.distance(this.value);
+    simulation.alpha(0.5).restart();  // Re-heat the simulation
+  });
+
+  d3.select("#mbSterngth").on("input", function() {
+    manyBody.strength(this.value);
+    simulation.alpha(0.5).restart();  // Re-heat the simulation
+  });
+
+  let linkForce = d3.forceLink()
+    .id(function(d) { return d.index; })
+    .strength(2)
+    .distance(1);
+
+  let manyBody = d3.forceManyBody()
+    .strength(2);
+
+  let simulation = d3.forceSimulation().nodes(nodes)
+    .force("link", linkForce)
+    .force("charge", manyBody);
 
   let svg = d3.select('force').append('svg')
     .attr('width', width)
@@ -112,13 +85,6 @@ let force = function(route, distance = 1, strength = 2) {
     .data(links)
     .enter().append('line')
       .attr("stroke-width", 1);
-
-  d3.select("#strengthElem").on("input", function() {
-    strength = this.value;
-    simulation.alphaTarget(0.3).restart();
-  });
-
-  console.log('force', force.strength);
 
   let node = svg.append("g")
       .attr("class", "nodes")
@@ -165,5 +131,3 @@ let force = function(route, distance = 1, strength = 2) {
 };
 
 module.exports.force = force;
-module.exports.clearForces = clearForces;
-
