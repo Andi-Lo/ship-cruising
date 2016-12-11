df1e0e552da78b6639ed3ecb1e2c29fa5162dcfb
Andi-Lo <contact@andreaslorer.de>
2016-12-11 19:45:27 +0100

    Added first tool draft

---
+++ src/tool/js/tool.js
@@ -0,0 +1,25 @@
+
+var toolMap = $('.tool-wrapper');
+var content = $(toolMap).find('.tool-content');
+
+var scrubber = document.getElementById('scrubber');
+console.log('scrubber', scrubber);
+
+scrubber.addEventListener('mousedown', function(evt) {
+  document.addEventListener('mousemove', setHeight, false);
+}, false);
+
+document.addEventListener('mouseup', function(evt) {
+  document.removeEventListener('mousemove', setHeight, false);
+}, false);
+
+function setHeight(evt) {
+  var y = evt.clientY
+  if(evt.clientY < 15) {
+    $(toolMap).css('height', 0);
+    $(content).css('height', 0);
+  }  else {
+    $(toolMap).css('height', y);
+    $(content).css('height', y);
+  }
+}
