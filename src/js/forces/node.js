02b7b9f72bb81fd8a3a927ffa72568d11716cebd
Andi-Lo <contact@andreaslorer.de>
2016-12-17 12:14:19 +0100

    should fix #20 and #34

--- src/js/forces/node.js
+++ src/js/forces/node.js
@@ -5,45 +5,50 @@ let L = require('leaflet');
 let options = require('../modules/options').force;
 
 class Node {
-  constructor() {
-
+  constructor(coords, latLng, radius = 3) {
+    this._x = coords.x,
+    this._y = coords.y,
+    this._latLng = latLng;
+    this._radius = radius;
+    this._color = options.nodeColor;
+    this._isLand = false;
+    return this;
   }
 
-  static createNode(coords, latLng, hexColor, isLand, radius = 3) {
-    return {
-      radius: radius,
-      x: coords.x,
-      y: coords.y,
-      latLng: latLng,
-      color: hexColor,
-      isLand: isLand
-    };
-  };
+  set color(val) { this._color = val; }
+  get color() { return this._color; }
+
+  set x(val) { this._x = val; }
+  get x() { return this._x; }
+
+  set y(val) { this._y = val; }
+  get y() { return this._y; }
+
+  set latLng(val) { this._latLng = val; }
+  get latLng() { return this._latLng; }
+
+  set radius(val) { this._radius = val; }
+  get radius() { return this._radius; }
+
+  get isLand() { return this._isLand; }
 
   static getNodes(route, map) {
     let node = [];
     let i = 0;
-    turf.meta.coordEach(route, function(coord) {
-      // let pixel = mercator.posToPixel(coord);
-      let latLng = new L.LatLng(coord[1], coord[0]);
-      let pixel = Node.projectPoint(coord[0], coord[1], map);
-      if('geometry' in route) {
-        if(i === 0 || i === route.geometry.coordinates.length - 1) {
-          node.push(Node.createNode(pixel, latLng, options.nodeColor, false));
+
+    turf.meta.featureEach(route, function(feature) {
+      turf.meta.coordEach(feature, function(coord) {
+        let latLng = new L.LatLng(coord[1], coord[0]);
+        let pixel = Node.projectPoint(coord[0], coord[1], map);
+        node.push(new Node(pixel, latLng));
+        if(i === 0 || i === feature.geometry.coordinates.length - 1) {
           node[i].fx = pixel.x;
           node[i].fy = pixel.y;
         }
-        else {
-          node.push(Node.createNode(pixel, latLng, options.nodeColor, false));
-        }
-      }
-      else {
-        node.push(Node.createNode(pixel, latLng, options.landColor, true, 2));
-        node[i].fx = pixel.x;
-        node[i].fy = pixel.y;
-      }
-      ++i;
+        ++i;
+      });
     });
+
     return node;
   };
 
